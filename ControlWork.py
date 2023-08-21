ade1c67bf9b74678dddb482fd2c34259e8f7bee6
Dmitry18 <bgvdima@yandex.ru>
2023-08-21 17:12:26 +0400

    add function search and add notes 2

--- ControlWork.py
+++ ControlWork.py
@@ -18,57 +18,71 @@ import datetime
 from time import strftime
 import json
 
-file_path = "notes.json"
+file = "notes.json"
 def vvod():
     vizov = int (input('Введите команду: \n1 - Вывод всех данных \n2 - Добавление заметки \n3 - Поиск заметки \n4 - Удаление заметки \n5 - Редактировние заметки \n:'))
     return vizov
 
-def vivod_dannih(file_path):
-    with open(file_path, 'r', encoding='UTF-8') as f:
+def vivod_dannih(file):
+    with open(file, 'r', encoding='UTF-8') as f:
         json_data = json.load(f)
         # print (type(json_data))
         # print(json_data["notes"])
         for item in json_data["notes"]:
-            print (f'{item["id"]}; {item["header"]:>8}; {item["body"]:>15}; {item["noteData"]}')
+            print (f'{item["id"]}; {item["header"]:>8}; {item["body"]:>20}; {item["noteData"]:>15} {item["noteTime"]:>10}')
+    f.close()
     return True
 
-def add_contact(file_path):
-    with open(file_path, 'a', encoding='UTF-8') as f:
-        id = input('Введите идентификатор id: ')
-        header = input('Заголовок заметки: ')
-        body = input('Тело заметки: ')
-        noteData = strftime("%Y-%m-%d-%H.%M.%S") # 2017-04-05-00.18.00
-        print(f'Контакт успешно добавлен --> {id} {header} {body} {noteData}')
-        f.write ('\n' + f'{id};{header};{body};{noteData}')
+def add_note(file):
+    input_id = input('Введите идентификатор id: ')
+    input_header = input('Заголовок заметки: ')
+    input_body = input('Тело заметки: ')
+    input_noteData = strftime("%Y.%m.%d") # 2017.04.05
+    input_noteTime = strftime("%H:%M:%S") # 00:18:00
+    new_data = {"id": input_id, "header": input_header, "body": input_body, "noteData": input_noteData, "noteTime": input_noteTime}
+    with open(file, mode='r', encoding='UTF-8') as f:
+        data = json.load(f)
+        data["notes"].append(new_data)
+        with open(file, mode='w', encoding= 'UTF-8') as f:
+            json.dump(data, f, ensure_ascii= False, indent=2)
+        print("Запись успешно добавлена")
+    f.close()
     return True
 
-def search_cont(file_path):
-    with open(file_path, 'r', encoding='UTF-8') as f:
-        poisk = input('Введите данные контакта: ')
-        for line in f:
-            if poisk in line:
-                mass = line.strip().split(';')
-                for i in range(len(mass)):
-                    print(mass[i], end=" ")
-                print("")
-        f.read()
+def search_note(file):
+    with open(file, mode='r', encoding='UTF-8') as f:
+        data = json.load(f)
+        poisk = input('Введите данные для поиска: ')
+        for item in data["notes"]:
+            if (str(item ["noteData"]).__contains__(poisk)):
+                print(item)
+    f.close()
     return True
 
-def del_data (file_path):
-    with open(file_path, 'r', encoding='UTF-8') as fi:
-        lines = fi.readlines()
-        fi.close()
-    with open(file_path, 'w', encoding='UTF-8') as f:
-        poisk = input('Введите данные заметки для удаления: ')
-        for line in lines:
-            if poisk not in line:
-                f.write(line)
+def del_data (file):
+    with open(file, 'r', encoding='UTF-8') as f:
+        json_data = json.load(f)
+        vvod_id = int (input('Введите id записи для удаления: '))
+        for item in json_data["notes"]:
+            # print(item["id"])
+            if vvod_id == int(item["id"]):
+                print (f'Выбрана запись для удаления: \n {item["id"]}; {item["header"]:>8}; {item["body"]:>15}; {item["noteData"]}')
+                vvod_param = int (input('Подтвердите удаление записи: \n1 - Подтвердить \n2 - Отменить \n'))
+                if vvod_param == 1:
+                    del item;
+                    print (item["id"])
         f.close()
+    # with open(file, 'w', encoding='UTF-8') as f:
+    #     poisk = input('Введите данные заметки для удаления: ')
+    #     for line in lines:
+    #         if poisk not in line:
+    #             f.write(line)
+    #     f.close()
     print("Заметка успешно удалена")
     return True
 
-def edit_data (file_path):
-    with open(file_path, 'r', encoding='UTF-8') as f:
+def edit_data (file):
+    with open(file, 'r', encoding='UTF-8') as f:
         json_data = json.load(f)
         vvod_id = int (input('Введите id записи для редактирования: '))
         for item in json_data["notes"]:
@@ -78,22 +92,26 @@ def edit_data (file_path):
                 vvod_param = int (input('Введите параметр записи для редактирования: \n1 - id заметки \n2 - Заголовок заметки \n3 - Тело заметки \n'))
                 if vvod_param == 1:
                     print (item["id"])
+
+                elif vvod_param == 2:
+                    print (item ["header"])
     print("Заметка успешно изменена")
+    f.close()
     return True
 
 
 input_ = vvod()
 
 if input_ ==1:
-    vivod_dannih(file_path)
+    vivod_dannih(file)
 elif input_ ==2:
-    add_contact(file_path)
+    add_note(file)
 elif input_ ==3:
-    search_cont(file_path)
+    search_note(file)
 elif input_ ==4:
-    del_data(file_path)
+    del_data(file)
 elif input_ ==5:
-    edit_data(file_path)
+    edit_data(file)
 else:
     print("Выбран недопустимый параметр! Попробуйте снова! ")
 
